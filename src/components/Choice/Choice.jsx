af89065cd221060fec105d4cf9ae339246f9ae22
Nilesh Sharma <nileshnilesharma@gmail.com>
2022-06-16 20:17:29 +0530

    Updated cursor and landing page

--- src/components/Choice/Choice.jsx
+++ src/components/Choice/Choice.jsx
@@ -4,7 +4,11 @@ import "./Choice.css"
 function Choice() {
   return (
     <div className='choice-container'>
-        <img className='choice-img' src="images/B.t.png" alt = "test"/>
+      <div className="choice-box">
+      <img className='choice-img boobs' src="images/Chesticles(1).png" alt = "test"/>
+      <img className='choice-img butt' src="images/B.t(1).png" alt = "test"/>
+      </div>
+        
     </div>
   )
 }
--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,10 +1,63 @@
-import React from 'react'
+import {useState, useRef, useEffect} from 'react'
 import './LandingPage.css'
+import {motion} from "framer-motion"
 
 function LandingPage() {
+  const leftCheekRef = useRef(null)
+  
+
+  const [mousePosition, setMousePosition] = useState({
+    x:0,
+    y:0
+  })
+
+  const variants = {
+    default: {
+      x: mousePosition.x+ 2,
+      y: mousePosition.y - 16
+    }
+  }
+
+
+  useEffect(()=>{
+    const mouseMove= e =>{
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY
+      })
+    }
+
+    window.addEventListener("mousemove",mouseMove)
+
+    return () => { 
+      window.removeEventListener("mousemove",mouseMove)
+    }
+  },[])
+
   return (
     <div className='landing-container'>
-        LandingPage
+        <img className='landing-img butt-land' src="images/bluebutt.png" alt = "bluebutt"/>
+        <img className='landing-img butt-half-land' ref={leftCheekRef} src="images/bluebutt_left.png" alt = "bluebutt-half"/>
+        {/* <motion.div className='fake-drag'
+               > */}
+          <motion.img 
+              drag="x" 
+              // dragConstraints={leftCheekRef}
+              dragElastic={0} 
+              whileDrag={{scale: 0.9}}
+              className='joint-land' 
+              src="images/Only_joint(1).png" 
+              alt = "joint"/>
+          {/* </motion.div> */}
+
+        <motion.img
+          src='images/L.t.1(1).png' 
+          className='cursor'
+          variants={variants}
+          animate="default"
+        />
+
+        {/* <div className='cursor'/> */}
     </div>
   )
 }
