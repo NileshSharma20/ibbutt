587687f8ce2dc2809f938afeca82151640a3282a
Nilesh Sharma <nileshnilesharma@gmail.com>
2022-06-17 21:09:25 +0530

    Added animation

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -3,12 +3,22 @@ import './LandingPage.css'
 import {motion} from "framer-motion"
 
 function LandingPage() {
-  const leftCheekRef = useRef(null)
-  const jointRef = useRef(null)
-  const fakeFillerRef = useRef(null)
+  const leftCheekRef = useRef()
+  const jointRef = useRef()
+  const fakeFillerRef = useRef()
+  const lconRef = useRef()
   
   const [cheekWidth, setCheekWidth] = useState(0);
-  const [jointX, setJointX] = useState();
+  const [fakeRefX, setFakeRefX] = useState();
+  const [lconWidth, setLconWidth] = useState();
+  const [diff, setDiff] = useState(0)
+  const [logicCounter, setLogicCounter] = useState(1)
+  const [isAnimating, setIsAnimating] = useState(false)
+  const [smoke, setSmoke] = useState(false)
+
+  const [cursorVarint, setCursorVariant] = useState("default")
+
+
   const [mousePosition, setMousePosition] = useState({
     x:0,
     y:0
@@ -16,16 +26,42 @@ function LandingPage() {
 
   const variants = {
     default: {
-      x: mousePosition.x+ 2,
+      x: mousePosition.x+ 5,
       y: mousePosition.y - 16
     }
+
+    // burnt:{
+
+    // }
   }
 
-  const getJointPosition=()=>{
-    var x = jointRef.current.offsetLeft;
-    setJointX(x);
+  const getFakeRefPosition = () => {
+    const fx = fakeFillerRef.current.offsetLeft;
+    console.log(`fakeRef: ${fakeFillerRef.current.offsetLeft}`)
+    setFakeRefX(fx);
   }
 
+  const getLconWidth = () => {
+    const lw = lconRef.current.offsetWidth
+    console.log(`lw: ${lw}`)
+    
+    setLconWidth(lw)
+  }
+
+  const handleImgClick=()=>{
+    setIsAnimating(true)
+    if(logicCounter!==0){
+      setLogicCounter(logicCounter-1)
+    }
+    else(
+      setSmoke(true)
+    )
+    
+  }
+
+  useEffect(()=>{
+    console.log(`${isAnimating}`)
+  },[isAnimating])
 
 
   useEffect(()=>{
@@ -34,9 +70,10 @@ function LandingPage() {
   },[cheekWidth])
 
   useEffect(()=>{
-    console.log(`jointX: ${jointX}`)
-    getJointPosition();
-  },[jointX])
+    // console.log(`jointX: ${jointRef.current.offsetLeft}`)
+      getFakeRefPosition();
+
+  },[])
 
   useEffect(()=>{
     const mouseMove= e =>{
@@ -46,14 +83,18 @@ function LandingPage() {
       })
     }
 
-    getJointPosition();
+    getFakeRefPosition();
+    getLconWidth()
 
-    window.addEventListener("mousemove",mouseMove, getJointPosition)
+    window.addEventListener("mousemove",mouseMove)
 
-    window.addEventListener("resize", getJointPosition);
+    window.addEventListener("resize", getLconWidth);
+    window.addEventListener("resize", getFakeRefPosition)
 
     return () => { 
-      window.removeEventListener("mousemove",mouseMove, getJointPosition)
+      window.removeEventListener("mousemove",mouseMove)
+      window.removeEventListener("resize", getLconWidth);
+      window.removeEventListener("resize", getFakeRefPosition)
     }
   },[])
 
@@ -61,45 +102,62 @@ function LandingPage() {
     const { offsetHeight, offsetWidth } = img;
     setCheekWidth(offsetWidth)
     console.log(offsetHeight, offsetWidth);
+    console.log(`diff: ${lconWidth/2 - cheekWidth}`)
+    setDiff(lconWidth/2 - cheekWidth - lconWidth*0.25)
   };
 
   return (
-    <div className='landing-container'>
+    <div className='landing-container' ref={lconRef}>
         
         <img className='landing-img butt-land' 
           src="images/lightbutt.png" 
-          alt = "bluebutt"/>
-        
-        <div className="fake-space">
+          alt = "lightbutt"/>
           
           <img className='butt-half-land' 
             ref={leftCheekRef} 
             onLoad={onImgLoad}
             src="images/lightbutt_left.png" 
             alt = "lightbutt-half"/>
+
           
           <div className="fake-filler" ref={fakeFillerRef}></div>
-        
-        </div>
-        
-        <motion.img 
-            drag="x" 
-            dragConstraints={fakeFillerRef}
-            dragElastic={0} 
-            dragMomentum={false}
-            whileDrag={{scale: 0.95}}
-            className='joint-land' 
-            ref = {jointRef}
-            src="images/Only_joint(1).png" 
-            alt = "joint"/>
 
+        <motion.div className="img-wrapper"
+          initial={{x:0}}
+          animate={{
+            x:isAnimating?-diff:0, 
+            }}
+          transition={{type:"tween", duration:2}}
+          onClick={()=>handleImgClick()}
+          ref = {jointRef}
+        >
+        <img className='joint-land' 
+            src="images/Only_joint(1).png" 
+            alt = "joint"
+            />
+          {isAnimating&&smoke?
+            <motion.img className='smoke-img' 
+              src= "images/smoke.png" 
+              alt="smoke"
+              initial={{opacity:0}}
+              animate={{opacity:1}}
+              transition={{duration:0.5}} />
+            :<></>}
+        </motion.div>
+        {isAnimating?
+        <motion.img
+          src='images/L.t.2(1).png' 
+          className='cursor'
+          variants={variants}
+          animate={cursorVarint}
+          />
+        :
         <motion.img
           src='images/L.t.1(1).png' 
           className='cursor'
           variants={variants}
-          animate="default"/>
-
-        {/* <div className='cursor'/> */}
+          animate={cursorVarint}
+          />}
     </div>
   )
 }
