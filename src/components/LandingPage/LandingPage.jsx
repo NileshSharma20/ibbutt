f8780595a316d5f4ae866b447b13975104f4f56c
Nilesh Sharma <nileshnilesharma@gmail.com>
2022-06-17 12:19:57 +0530

    Added ref boundaries for drag

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -4,8 +4,11 @@ import {motion} from "framer-motion"
 
 function LandingPage() {
   const leftCheekRef = useRef(null)
+  const jointRef = useRef(null)
+  const fakeFillerRef = useRef(null)
   
-
+  const [cheekWidth, setCheekWidth] = useState(0);
+  const [jointX, setJointX] = useState();
   const [mousePosition, setMousePosition] = useState({
     x:0,
     y:0
@@ -18,6 +21,22 @@ function LandingPage() {
     }
   }
 
+  const getJointPosition=()=>{
+    var x = jointRef.current.offsetLeft;
+    setJointX(x);
+  }
+
+
+
+  useEffect(()=>{
+    console.log(`cheek width: ${leftCheekRef.current.offsetWidth}`)
+    setCheekWidth(leftCheekRef.current.offsetWidth)
+  },[cheekWidth])
+
+  useEffect(()=>{
+    console.log(`jointX: ${jointX}`)
+    getJointPosition();
+  },[jointX])
 
   useEffect(()=>{
     const mouseMove= e =>{
@@ -27,35 +46,58 @@ function LandingPage() {
       })
     }
 
-    window.addEventListener("mousemove",mouseMove)
+    getJointPosition();
+
+    window.addEventListener("mousemove",mouseMove, getJointPosition)
+
+    window.addEventListener("resize", getJointPosition);
 
     return () => { 
-      window.removeEventListener("mousemove",mouseMove)
+      window.removeEventListener("mousemove",mouseMove, getJointPosition)
     }
   },[])
 
+  const onImgLoad = ({ target: img }) => {
+    const { offsetHeight, offsetWidth } = img;
+    setCheekWidth(offsetWidth)
+    console.log(offsetHeight, offsetWidth);
+  };
+
   return (
     <div className='landing-container'>
-        <img className='landing-img butt-land' src="images/bluebutt.png" alt = "bluebutt"/>
-        <img className='landing-img butt-half-land' ref={leftCheekRef} src="images/bluebutt_left.png" alt = "bluebutt-half"/>
-        {/* <motion.div className='fake-drag'
-               > */}
-          <motion.img 
-              drag="x" 
-              // dragConstraints={leftCheekRef}
-              dragElastic={0} 
-              whileDrag={{scale: 0.9}}
-              className='joint-land' 
-              src="images/Only_joint(1).png" 
-              alt = "joint"/>
-          {/* </motion.div> */}
+        
+        <img className='landing-img butt-land' 
+          src="images/lightbutt.png" 
+          alt = "bluebutt"/>
+        
+        <div className="fake-space">
+          
+          <img className='butt-half-land' 
+            ref={leftCheekRef} 
+            onLoad={onImgLoad}
+            src="images/lightbutt_left.png" 
+            alt = "lightbutt-half"/>
+          
+          <div className="fake-filler" ref={fakeFillerRef}></div>
+        
+        </div>
+        
+        <motion.img 
+            drag="x" 
+            dragConstraints={fakeFillerRef}
+            dragElastic={0} 
+            dragMomentum={false}
+            whileDrag={{scale: 0.95}}
+            className='joint-land' 
+            ref = {jointRef}
+            src="images/Only_joint(1).png" 
+            alt = "joint"/>
 
         <motion.img
           src='images/L.t.1(1).png' 
           className='cursor'
           variants={variants}
-          animate="default"
-        />
+          animate="default"/>
 
         {/* <div className='cursor'/> */}
     </div>
