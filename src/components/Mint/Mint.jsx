107c4d6b0236578f36cbc75ea3c24eccc2524b83
Nilesh Sharma <nileshnilesharma@gmail.com>
2022-06-24 12:20:41 +0530

    Mint page

--- src/components/Mint/Mint.jsx
+++ src/components/Mint/Mint.jsx
@@ -3,7 +3,17 @@ import './Mint.css'
 
 function Mint() {
   return (
-    <div className='mint-container'>Mint</div>
+    <div className='mint-container'>
+      <div className="mint-content">
+        <div className="mint-info">Mint-info</div>
+        <div className="mint-btn">Mint</div>
+      </div>
+      <div className="social-grid">
+        <img src ="images/Discord_j.png" alt="discord" />
+        <img src ="images/Twitter_j.png" alt="twitter" />
+        <img src ="images/Opensea_j.png" alt="opeansea" />
+      </div>
+    </div>
   )
 }
 
