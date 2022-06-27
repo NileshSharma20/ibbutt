09db82e274933e1ff8a5c8d2dc123617906c6e00
Nilesh Sharma <nileshnilesharma@gmail.com>
2022-06-27 11:33:07 +0530

    Show

--- src/components/Mint/Mint.jsx
+++ src/components/Mint/Mint.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
+import { useMediaQuery } from 'react-responsive'
 import './Mint.css'
 
 function Mint() {
+  const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
+
   return (
     <div className='mint-container'>
+      
       <div className="mint-content">
         <div className="mint-info">Mint-info</div>
         <div className="mint-btn">Mint</div>
       </div>
+      
       <div className="social-grid">
-        <img src ="images/Discord_j.png" alt="discord" />
-        <img src ="images/Twitter_j.png" alt="twitter" />
-        <img src ="images/Opensea_j.png" alt="opeansea" />
+        <img className={`mint-img ${isPortrait ?"mint-img-mobile":""}`} 
+          src ="images/Discord_j.png" 
+          alt="discord" />
+        
+        <img className={`mint-img ${isPortrait ?"mint-img-mobile":""}`}
+          src ="images/Twitter_j.png" 
+          alt="twitter" />
+        
+        <img className={`mint-img ${isPortrait ?"mint-img-mobile":""}`}
+          src ="images/Opensea_j.png" 
+          alt="opeansea" />
       </div>
+    
     </div>
   )
 }
