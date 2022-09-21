2b8c4c15bf7fc7674479b154ab68175528105cc2
Nilesh Sharma <nileshnilesharma@gmail.com>
2022-09-21 10:36:47 +0530

    Welcome page updted

--- src/components/Poster.jsx
+++ src/components/Poster.jsx
@@ -6,23 +6,25 @@ function Poster() {
     <div className='poster-box'>
         
         <div className='poster-mid-section'>
-            <h1 style={{fontWeight:"600", fontFamily:"Didot"}} >The whole concept was made on drugs.</h1>
+            <h1 style={{fontWeight:"600", fontFamily:"Didot", fontSize:"2rem"}} >The whole concept was made on drugs.</h1>
             <div className="underline"></div>
         </div>
 
         <div className='poster-mid-section'>
+            
             <div className="poster-details-img">
-            Image here
+                <img style={{objectFit:"contain", width:"100%", height:"auto"}} src='https://ik.imagekit.io/qljqw3tvn/ibb/poster-butt.png' alt="" />
             </div>
+
             <div className="poster-details">
                 <div className="poster-details-para">
-                    <span style={{fontWeight:"600", fontFamily:"Didot"}}>We are reserving 210 asses (4.2%).</span> Because we like getting high and fucked.
+                    <span style={{fontWeight:"600", fontFamily:"Didot"}}>We are reserving 210 asses (4.2%).</span> Because we like getting high and getting fucked.
                     
                     <div className="underline"></div>
                 </div>
                 
                 <div className="poster-details-para">
-                <span style={{fontWeight:"600", fontFamily:"Didot"}}>CCO.</span> Discord available for those baked out of their mids lol.
+                <span style={{fontWeight:"600", fontFamily:"Didot"}}>CCO.</span> Discord available for those baked out of their minds lol.
                 </div>
             </div>
             <div className='underline'></div>
@@ -30,7 +32,7 @@ function Poster() {
 
         
         <div className='poster-mid-section'>
-            <h1 style={{fontFamily:"Didot", fontWeight:"400"}}><a style={{fontWeight:"600", fontFamily:"Didot"}}>Contract</a> was actually written after our developer mixed LSD and Coke and boomed the fuck outta it.</h1>
+            <h1 style={{fontFamily:"Didot", fontWeight:"400"}}><a style={{fontWeight:"600", fontFamily:"Didot", color:"blue", cursor:"pointer"}}>Contract</a> was actually written after our developer mixed LSD and Coke and boomed the fuck outta it.</h1>
         </div>
     </div>
   )
--- src/pages/ClassyButt/ClassyButt.jsx
+++ src/pages/ClassyButt/ClassyButt.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { useMediaQuery } from 'react-responsive'
 import { isMobile } from 'react-device-detect';
 import { useNavigate } from 'react-router-dom'
@@ -10,9 +10,15 @@ import Poster from '../../components/Poster';
 function ClassyButt() {
   const navigate = useNavigate();
 
+  const posterRef = useRef(null);
+
   const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
 
   const [mintFlag, setMintFlag] = useState(false)
+
+  const handleScroll =()=>{
+    window.scrollTo({top:posterRef.current.offsetTop, behavior:"smooth"})
+  }
 
   useEffect(()=>{
       if(mintFlag){
@@ -24,15 +30,15 @@ function ClassyButt() {
     <>
     <div className='make-container'>
       <div className="grid-layout">
-          <div className="poster-container poster-info">
-            {/* <img className={`comic-img poster-img ${isPortrait?"comic-img-portrait":""}`}  
-              src='images/nft-show.gif' alt="comic"/> */}
+          {/* <div className="poster-container poster-info">
+            <img className={`comic-img poster-img ${isPortrait?"comic-img-portrait":""}`}  
+              src='images/nft-show.gif' alt="comic"/>
             
-            {/* <div className={`goto-mint-btn ${isMobile? "goto-mint-btn-mobile":""}`} 
+            <div className={`goto-mint-btn ${isMobile? "goto-mint-btn-mobile":""}`} 
               onClick={()=>setMintFlag(true)}>
                 Get your ass
-            </div> */}
-          </div>
+            </div>
+          </div> */}
         
         <img className={`failed-text-swine ${isPortrait?"failed-text-mobile-success":""}`} 
             src="https://ik.imagekit.io/qljqw3tvn/ibb/splendor-small.gif" 
@@ -42,24 +48,25 @@ function ClassyButt() {
         <img className={`classy-butt ${isPortrait ? "classy-butt-mobile":""}`} 
           src="https://ik.imagekit.io/qljqw3tvn/ibb/MrButt_418OwtD8k.png" alt = "test"/>
       
-      <div className="scroll"></div>
+      
       </div>
       
-
+      
       <Socials />
     </div>
 
-    <div className='make-container'>
+    <div className='make-container' ref={posterRef}>
         <div className="grid-layout">
           
           <div className="poster-container poster-info">
-            <img className={`comic-img poster-img ${isPortrait?"comic-img-portrait":""}`}  
+            {/* <img className={`comic-img poster-img ${isPortrait?"comic-img-portrait":""}`}  
                 src="https://ik.imagekit.io/qljqw3tvn/ibb/poster_zp_z6LwlR.png" 
-                alt="comic"/>
-
+                alt="comic"/> */}
+            <Poster />
             <div className={`goto-mint-btn ${isMobile? "goto-mint-btn-mobile":""}`} 
                 onClick={()=>setMintFlag(true)}>
                   <img className='try-again' src='https://ik.imagekit.io/qljqw3tvn/ibb/getYourAssBtn_80wwRhfcw.gif' alt='Get your ass' />
+                  {/* <Poster /> */}
             </div>
               
             </div>
@@ -75,48 +82,15 @@ function ClassyButt() {
         </div>
     </div>
 
-    <div className="make-container">
+    {/* <div className="make-container">
       <div className="grid-layout">
         <Poster />
       </div>
-    </div>
-
-    {/* <div className='make-container'>
-        <div className="grid-layout">
-          
-          <div className="poster-container">
-            <img className={`comic-img ${isPortrait?"comic-img-portrait":""}`} src='images/Comic.png' alt="comic"/>
-            
-          </div>
-          <div className="poster-container">
-            <img className={`failed-text ${isPortrait?"failed-text-mobile":""}`} 
-              src="images/splendour.gif" 
-              alt = "test"/>
-          
-                
-            <img className={`classy-butt ${isPortrait ? "classy-butt-mobile":""}`} src="images/B.t .2.png" alt = "test"/>
-          </div>
-
-          <div className="poster-container">
-            <img className={`comic-img ${isPortrait?"comic-img-portrait":""}`} src='images/nft-show.gif' alt="comic"/>
-          </div>
-          
-          <div className="poster-container poster-info"> 
-            
-            <img className={`comic-img poster-img ${isPortrait?"comic-img-portrait":""}`} 
-              src='images/Poster.png' 
-              alt="comic"/>
-            
-            <div className={`goto-mint-btn ${isMobile? "goto-mint-btn-mobile":""}`} 
-              onClick={()=>setMintFlag(true)}>
-                Get your ass
-            </div>
-            
-          </div>
-        </div>
-        
-        <Socials />
     </div> */}
+
+    <div className="scroll" 
+        onClick={()=>handleScroll()}
+        ></div>
     </>
   )
 }
