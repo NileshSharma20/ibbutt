f8fe293796d0c91ccc71164dfdbe675031f6fcbd
Nilesh Sharma <nileshnilesharma@gmail.com>
2022-09-23 14:52:29 +0530

    Trial1

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -292,7 +292,7 @@ function LandingPage() {
             <source src="https://ik.imagekit.io/qljqw3tvn/ibb/psy-back.mp4"
               type="video/mp4"/>
       </video>
-    <div className={`landing-container  ${isPortrait? "landing-mobile":""}`} ref={lconRef}>
+    <div className={`landing-container`} ref={lconRef}>
         {!isAnimating && 
         <div className='text-box'>
           <img className='text-box-img' src='https://ik.imagekit.io/qljqw3tvn/ibb/click-to-put-opt.gif' alt='Put it in'/>
